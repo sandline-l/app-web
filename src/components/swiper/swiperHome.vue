7bc4c5c30c3438f556ae23e189692588a72f5d8b
unknown <1610826934@qq.com>
2019-06-22 18:28:56 +0800

    upload

--- src/components/swiper/swiperHome.vue
+++ src/components/swiper/swiperHome.vue
@@ -1,36 +1,45 @@
 <template>
   <div class="swiper">
     <el-carousel height="150px">
-      <el-carousel-item v-for="(item,index) in imgList" :key="index">
-          <img :src="item.url" alt="">
-          <h1>{{index}}</h1>
-          <h2>{{item.url}}</h2>
+      <el-carousel-item >
+          <img src="@/assets/img/daojishi.jpg" alt="">
+      </el-carousel-item>
+      <el-carousel-item >
+          <img src="@/assets/img/guanggaoyz.jpg" alt="">
+      </el-carousel-item>
+      <el-carousel-item >
+          <img src="@/assets/img/guanggaoxd.jpg" alt="">
+      </el-carousel-item>
+      <el-carousel-item >
+          <img src="@/assets/img/wangjing2.jpg" alt="">
       </el-carousel-item>
     </el-carousel>
   </div>
 </template>
 
 <script>
+// import imglist from '@/assets/img/daojishi.jpg'
 export default {
   data() {
     return {
-      msg: "",
-      imgList:[
-          {
-              url:'@/assets/img/daojishi.jpg'
-          },
-          {
-              url:'@/assets/img/guangao-xd.jpg'
-          },
-          {
-              url:'@/assets/img/guangao-yz.jpg'
-          },
-          {
-              url:'@/assets/img/wangjing-2.jpg'
-          }
-      ]
+      msg: "@/assets/img/daojishi.jpg",
+    //   imgList:[
+    //       {
+    //           url:'../../assets/img/daojishi.jpg'
+    //       },
+    //       {
+    //           url:'../../assets/img/guangao-xd.jpg'
+    //       },
+    //       {
+    //           url:'../../assets/img/guangao-yz.jpg'
+    //       },
+    //       {
+    //           url:'../../assets/img/wangjing-2.jpg'
+    //       }
+    //   ]
     };
-  }
+  },
+ 
 };
 </script>
 
